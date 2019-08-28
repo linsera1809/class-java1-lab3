131ed4863b2e87209915776e5ac5b69749b5d8bc
Linse, Bobby <LINSER1@nationwide.com>
2019-08-28 18:41:11 -0400

    adjusting Surname

---
+++ JavaIO/Given/src/edu/cscc/Surname.java
@@ -0,0 +1,50 @@
+package edu.cscc;
+
+public class Surname {
+    private String name;
+    private int rank;
+    private int count;
+    private double proportion;
+
+    // TODO - Add constructor
+    public Surname(String name, int rank, int count, double proportion) {
+        this.name = name;
+        this.rank = rank;
+        this.count = count;
+        this.proportion = proportion;
+    }
+
+
+    // TODO - Add getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public double getProportion() {
+        return proportion;
+    }
+
+    public void setProportion(double proportion) {
+        this.proportion = proportion;
+    }
+}
