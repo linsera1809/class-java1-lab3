e3a6e46a123fdd8c6d46a24b10a25e94883b2463
Linse, Bobby <LINSER1@nationwide.com>
2019-08-28 18:07:00 -0400

    Adding given proj files

---
+++ Lab3-Given/Lab9/src/edu/cscc/Census.java
@@ -0,0 +1,16 @@
+package edu.cscc;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Census {
+    public static Surname[] loadCensusData(String fname) {
+        int line = 0; // Keep track of lines of file we've read
+        Surname[] namelist = new Surname[100];
+
+        // TODO - Add code to read file and populate array of Surname objects
+
+        return namelist;
+    }
+}
---
+++ Lab3-Given/Lab9/src/edu/cscc/Surname.java
@@ -0,0 +1,13 @@
+package edu.cscc;
+
+public class Surname {
+    private String name;
+    private int rank;
+    private int count;
+    private double proportion;
+
+    // TODO - Add constructor
+
+    // TODO - Add getters and setters
+
+}
